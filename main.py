26ccc67db120e4be0767ba82e963df545623b98f
ivanovkd-2 <ivanov-konstantin-dm@yandex.ru>
2024-02-07 17:36:38 +0300

    pigs

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class studentik:
+    fio=''
+    score=0
+f=open('students.csv')
+students=[]
+f.readline()
+for i in f:
+    s=i.split(',')
+    if s[3][:-1]=='10':
+        a = studentik()
+        fi=s[1].split()
+        a.fio = fi[1][0]+'. '+fi[0]
+        a.score = int(s[4])
+        students.append(a)
+for i in range(len(students)):
+    j = i
+    t = students[i]
+    while j>0 and students[j-1].score<t.score:
+        students[j]=students[j-1]
+        j-=1
+    students[j]=t
+print('10 класс:')
+print('1 место:', students[0].fio)
+print('2 место:', students[1].fio)
+print('3 место:', students[2].fio)
